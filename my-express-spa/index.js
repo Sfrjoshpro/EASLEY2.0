c38035d158caa523ce570904bae5fbdd4774aa7a
Josh <sfrjoshpro@gmail.com>
2023-09-17 22:37:46 -0400

    fixed logout

--- my-express-spa/index.js
+++ my-express-spa/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const mysql = require('mysql2');
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const app = express();
 
 // Replace with your MySQL database connection details
@@ -10,6 +18,11 @@ const dbConfig = {
   port: '3306', // Default MySQL port is 3306
 };
 
+// Print the environment variables
+console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID);
+console.log('GOOGLE_CLIENT_SECRET:', process.env.GOOGLE_CLIENT_SECRET);
+console.log('SESSION_SECRET:', process.env.SESSION_SECRET);
+
 const dbName = 'easley'; // Change to your desired database name
 
 const connection = mysql.createConnection(dbConfig);
@@ -17,12 +30,32 @@ const connection = mysql.createConnection(dbConfig);
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Initialize Passport.js
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport.js serialization and deserialization
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
 // Define your route to handle other requests
 app.get('/', (req, res) => {
   res.send('Hello, this is your Node.js server!');
 });
 
-// Check if the database exists (similar to your previous code)
+// Check if the database exists
 connection.query(
   `SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${dbName}'`,
   (err, results) => {
@@ -32,7 +65,7 @@ connection.query(
     }
 
     if (results.length === 0) {
-      // The database doesn't exist; create it (similar to your previous code)
+      // The database doesn't exist; create it
       connection.query(`CREATE DATABASE ${dbName}`, (err) => {
         if (err) {
           console.error('Error creating database:', err);
@@ -45,6 +78,62 @@ connection.query(
     }
   }
 );
+
+// Passport.js Google OAuth configuration
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Handle user creation or authentication here
+      return done(null, profile);
+    }
+  )
+);
+
+// Google OAuth routes
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    // Successful authentication, redirect to the profile page
+    res.redirect('/profile');
+  }
+);
+
+// Profile route
+app.get('/profile', (req, res) => {
+  // Check if the user is authenticated
+  if (!req.isAuthenticated()) {
+    res.redirect('/'); // Redirect to the homepage if not authenticated
+    return;
+  }
+
+  // Access user details from the request object
+  const user = req.user;
+
+  // Display user information on the profile page
+  res.send(`
+    <h1>Welcome, ${user.displayName}</h1>
+    <img src="${user.photos[0].value}" alt="Profile Picture">
+    <p>Email: ${user.emails[0].value}</p>
+    <a href="/logout">Logout</a>
+  `);
+});
+
+// Logout route
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 // Start the server
 const port = 3000;
--- my-express-spa/public/app.js
+++ my-express-spa/public/app.js
@@ -2,15 +2,13 @@
 function loadContent(fragmentId) {
   const content = document.getElementById('content');
 
-  
-
   // Add a button to the content
   content.innerHTML += '<button id="myButton" class="btn btn-primary">Click Me</button>';
 
   // Handle button click
   const button = document.getElementById('myButton');
   button.addEventListener('click', function () {
-      alert('YAY!')
+      alert('YAY!');
   });
 }
 
@@ -25,3 +23,10 @@ window.addEventListener('hashchange', navigate);
 
 // Initial page load
 navigate();
+
+// Add event listener for the "Login with Google" button
+const googleLoginBtn = document.getElementById('googleLoginBtn');
+googleLoginBtn.addEventListener('click', () => {
+  // Redirect the user to the Google OAuth authentication URL
+  window.location.href = '/auth/google';
+});
